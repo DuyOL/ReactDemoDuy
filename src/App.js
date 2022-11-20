9a423713e1c10f727b14b61d551410a1009a880c
DuyOL <93581388+DuyOL@users.noreply.github.com>
2022-11-20 21:04:13 +0700

    Bai 17

--- src/App.js
+++ src/App.js
@@ -1,33 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
 import MyComponent from './components/MyComponent';
+import React from 'react';
 
-function App() {
-  return (
-    <div>
-      Hello world với Nguyễn Duy &amp; Thơ Ông Đệ
-      <MyComponent></MyComponent>
-    </div>
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Hello World React.Js (Nguyễn Đức Duy)
-    //     </p>
-    //     {/* <div>Count = {count}</div>
-    //     <button onclick={() => dispatch(increaseCounter())}>Increase</button>
-    //     <button onclick={() => dispatch(increaseCounter())}>Decrease</button> */}
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        Hello world với Nguyễn Duy &amp; Hải Dương
+        <MyComponent></MyComponent>
+      </div>
+
+    );
+  }
 }
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Hello World React.Js (Nguyễn Đức Duy)
+//         </p>
+//         {/* <div>Count = {count}</div>
+//         <button onclick={() => dispatch(increaseCounter())}>Increase</button>
+//         <button onclick={() => dispatch(increaseCounter())}>Decrease</button> */}
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -2,12 +2,17 @@
 // Function Component
 import React from "react";
 class MyComponent extends React.Component {
+
+    state = {
+        name: 'Nguyen Duy',
+        address: 'Hai Duong',
+        age: 20
+    }
     // JSX
     render() {
         return (
-            <div>my First Component
-
-                {Math.random()}
+            <div>
+                My name is {this.state.name} and I'am From {this.state.address}
             </div>
         );
     }
