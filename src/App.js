04776c184a0c69f1781d7770f204f2d6a4b00d13
DuyOL <93581388+DuyOL@users.noreply.github.com>
2022-11-20 17:21:59 +0700

    video 16

--- src/App.js
+++ src/App.js
@@ -1,27 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import MyComponent from './components/MyComponent';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World React.Js (Nguyễn Đức Duy)
-        </p>
-        {/* <div>Count = {count}</div>
-        <button onclick={() => dispatch(increaseCounter())}>Increase</button>
-        <button onclick={() => dispatch(increaseCounter())}>Decrease</button> */}
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      Hello world với Nguyễn Duy &amp; Thơ Ông Đệ
+      <MyComponent></MyComponent>
     </div>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Hello World React.Js (Nguyễn Đức Duy)
+    //     </p>
+    //     {/* <div>Count = {count}</div>
+    //     <button onclick={() => dispatch(increaseCounter())}>Increase</button>
+    //     <button onclick={() => dispatch(increaseCounter())}>Decrease</button> */}
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
   );
 }
 
---
+++ src/components/MyComponent.js
@@ -0,0 +1,16 @@
+// Class Component
+// Function Component
+import React from "react";
+class MyComponent extends React.Component {
+    // JSX
+    render() {
+        return (
+            <div>my First Component
+
+                {Math.random()}
+            </div>
+        );
+    }
+}
+
+export default MyComponent;
