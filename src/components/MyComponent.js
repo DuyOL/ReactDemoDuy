1877bdececbd52e3f9f4de6bd7cd3705ca53cd75
DuyOL <93581388+DuyOL@users.noreply.github.com>
2022-11-24 18:23:29 +0700

    bai 21

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -13,24 +13,37 @@ class MyComponent extends React.Component {
         // Khi click vào cửa sổ console sẽ hiện ra chữ  Click me my button
         // merge State => react class
         this.setState({
-            name: 'James'
-        })
-
-        this.setState({
+            name: 'James',
             age: Math.floor((Math.random() * 100) + 1)
         })
+
     }
     hendleMoverOver(event) {
         // console.log(event.pageX)
     }
     // JSX
+    hendleOnChangeInput = (event) => {
+        this.setState({
+            name: event.target.value
+        })
+    }
+    handleOnSubmit = (event) => {
+        event.preventDefault();
+        console.log(this.state)
+    }
     render() {
         return (
             <div>
                 My name is {this.state.name} and I'am From {this.state.age}
                 {/* <button onClick={this.hendleClick}>Click me</button> */}
-                <button onMouseOver={this.hendleMoverOver}>MoverMe</button>
-                <button onClick={(event) => { this.hendleClick(event) }}>Click Me</button>
+                {/* <button onClick={(event) => { this.hendleClick(event) }}>Click Me</button> */}
+                <form onSubmit={(event) => this.handleOnSubmit(event)}>
+                    <input
+                        type="text"
+                        onChange={(event) => this.hendleOnChangeInput(event)}
+                    />
+                    <button>Submit</button>
+                </form>
             </div>
         );
     }
