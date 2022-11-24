34c69a600d7a0b2023db45aba8e5cb59ef02b76c
DuyOL <93581388+DuyOL@users.noreply.github.com>
2022-11-24 17:25:06 +0700

    bai 20

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -8,22 +8,29 @@ class MyComponent extends React.Component {
         address: 'Hai Duong',
         age: 20
     }
-    hendleClick(event) {
-        console.log(" >>> Click me my button")
-        console.log("My name is", this.state.name)
+    hendleClick = (event) => {
+        console.log(" >>> Click me my button");
         // Khi click vào cửa sổ console sẽ hiện ra chữ  Click me my button
+        // merge State => react class
+        this.setState({
+            name: 'James'
+        })
+
+        this.setState({
+            age: Math.floor((Math.random() * 100) + 1)
+        })
     }
     hendleMoverOver(event) {
-        console.log(event.pageX)
+        // console.log(event.pageX)
     }
     // JSX
     render() {
         return (
             <div>
-                My name is {this.state.name} and I'am From {this.state.address}
+                My name is {this.state.name} and I'am From {this.state.age}
                 {/* <button onClick={this.hendleClick}>Click me</button> */}
                 <button onMouseOver={this.hendleMoverOver}>MoverMe</button>
-                <button onClick={this.hendleClick}>Click Me</button>
+                <button onClick={(event) => { this.hendleClick(event) }}>Click Me</button>
             </div>
         );
     }
