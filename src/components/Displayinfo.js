98cd5a99c8c045589845b46055415af994914c94
DuyOL <93581388+DuyOL@users.noreply.github.com>
2022-11-28 11:18:55 +0700

    video 24

--- src/components/Displayinfo.js
+++ src/components/Displayinfo.js
@@ -2,12 +2,19 @@ import React from 'react';
 class Displayinfo extends React.Component {
     render() {
         // destructuring array/object
-        const { name, age } = this.props;
+        const { listUsers } = this.props;
+        console.log(listUsers)
         // props => viết tắt properties
         return (
             <div>
-                <div>My Name is {name} </div>
-                <div> My age 20 {age}</div>
+                {listUsers.map((user) => {
+                    return (
+                        <div key={user.id}>
+                            <div>My Name is  {user.name} </div>
+                            <div>My age     {user.age}</div>
+                        </div>
+                    )
+                })}
             </div>
         )
     }
--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -4,17 +4,25 @@ import React from "react";
 import Displayinfo from "./Displayinfo";
 import UserInfo from "./UserInfor";
 class MyComponent extends React.Component {
-
+    state = {
+        listUsers: [
+            { id: 1, name: 'Nguyen Duc Duy', age: '20' },
+            { id: 2, name: 'James', age: '19' },
+            { id: 3, name: 'Roys', age: '30' },
+        ]
+    }
     render() {
-        const MyInfo = ['Duy', 'Love', '???'];
         // props => viết tắt properties
+        // DRY : Don't repeat youseft
         return (
             <div>
                 <UserInfo></UserInfo>
                 <br></br>
-                <Displayinfo name="Hoi Dan IT" age="20"></Displayinfo>
+                <Displayinfo
+                    listUsers={this.state.listUsers}
+                />
+                {/* <Displayinfo name="Hoi Dan IT" age="20" /> */}
                 <hr></hr>
-                <Displayinfo name="Jame" age={26} MyInfo={MyInfo} />
             </div>
         );
     }
