afc7adaeb06d362ee6d0fe3b92fc01632adf2b0c
DuyOL <93581388+DuyOL@users.noreply.github.com>
2022-12-01 15:21:54 +0700

    video 27

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 import MyComponent from './components/MyComponent';
 import React from 'react';
 class App extends React.Component {
--- src/components/Displayinfo.js
+++ src/components/Displayinfo.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import './Displayinfo.scss';
+
 class Displayinfo extends React.Component {
     state = {
         isShowlistUser: true
@@ -14,7 +16,7 @@ class Displayinfo extends React.Component {
         // console.log(listUsers)
         // props => viết tắt properties
         return (
-            <div>
+            <div className='display-infor-container'>
                 <div>
                     <span onClick={() => { this.handleShowHide() }}>
                         {this.state.isShowlistUser === true ? "Hide list users: " : "Show list users"}
@@ -25,7 +27,7 @@ class Displayinfo extends React.Component {
                         {listUsers.map((user) => {
                             return (
                                 <div key={user.id} className={+user.age > 18 ? "green" : "red"}>
-                                    <div>My Name is  {user.name} </div>
+                                    <div style={{ color: 'black', paddingTop: '50px' }}>My Name is  {user.name} </div>
                                     <div>My age      {user.age}</div>
                                 </div>
                             )
